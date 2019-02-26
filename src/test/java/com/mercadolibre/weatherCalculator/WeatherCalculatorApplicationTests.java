a03a4bfb1c97b4251ca82339152e17c494b08214
Castrucci <maximiliano.castrucci@igt.com>
2019-02-26 14:12:13 -0300

    added reportTest to have the big output fixed a "clock-wize" issue in planets

--- src/test/java/com/mercadolibre/weatherCalculator/WeatherCalculatorApplicationTests.java
+++ src/test/java/com/mercadolibre/weatherCalculator/WeatherCalculatorApplicationTests.java
@@ -5,12 +5,25 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.mercadolibre.weatherCalculator.calculator.WeatherCalculator;
+import com.mercadolibre.weatherCalculator.calculator.galaxy.GalaxyStatus;
+import com.mercadolibre.weatherCalculator.calculator.weather.WeatherReport;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherCalculatorApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testReport() {
+		WeatherCalculator calculator = new WeatherCalculator();
+		WeatherReport report = new WeatherReport();
+		int tenYears = 365 * 10;
+		
+		for (int i = 0; i < tenYears; i++) {
+			GalaxyStatus status = calculator.calculateWeather(i);
+			report.update(status);
+		}
+		System.out.println(report.outputReportResults());
 	}
 
 }
